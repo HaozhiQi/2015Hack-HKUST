e06d8d6752f878b73fbf4e090ffc7f70784e710f
Oh233 <macshqi@gmail.com>
2015-03-28 15:33:35 +0800

    uploaded file movable

--- submit.php
+++ submit.php
@@ -1,5 +1,20 @@
 <?php
+	$doc_path = realpath(dirname(__FILE__));
 
+	// echo "<pre>";
+	// var_dump($_FILES);
+	// echo "</pre>";
+	
+	// the name of target image file
+	$name = $_FILES["fileToUpload"]["name"];
+
+	if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $doc_path . '/' . basename($name))) {
+        print "Upload succeed";
+    } else {
+        print "Upload failed!";
+    }
+	
+	
 	$db = new SQLite3('my_database.db') or die('unable to connect to database');
 	$db->busyTimeout(5000);
 	//drop previous tables
@@ -7,19 +22,21 @@
 	$db->exec("DROP TABLE IF EXISTS boxes");
 	//create tables
 	$db->exec("CREATE TABLE images(
-				id INTEGER PRIMARY KEY, path varchar(255), 
-				request_count INTEGER)"
+				id INTEGER PRIMARY KEY,
+				image_dir varchar(255))"
 				) 
 				or die("create images failed");
 	
 	$db->exec("CREATE TABLE boxes(
 				id INTEGER PRIMARY KEY, 
 				positive_classification INTEGER, 
-				image_id INTEGER, first_point_X REAL, 
+				image_id INTEGER, 
+				first_point_X REAL, 
 				first_point_Y REAL, 
 				second_point_X REAL, second_point_Y REAL)"
 				)
 				or die("create boxes failed");
+	
 	
 	$db->close();
 	
