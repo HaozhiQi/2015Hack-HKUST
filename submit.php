81b004393624fe665febb3b7fcd90081bf9403ba
Oh233 <macshqi@gmail.com>
2015-03-28 15:35:39 +0800

    seperate initialization part to different file

--- submit.php
+++ submit.php
@@ -16,27 +16,6 @@
 	
 	
 	$db = new SQLite3('my_database.db') or die('unable to connect to database');
-	$db->busyTimeout(5000);
-	//drop previous tables
-	$db->exec("DROP TABLE IF EXISTS images");
-	$db->exec("DROP TABLE IF EXISTS boxes");
-	//create tables
-	$db->exec("CREATE TABLE images(
-				id INTEGER PRIMARY KEY,
-				image_dir varchar(255))"
-				) 
-				or die("create images failed");
-	
-	$db->exec("CREATE TABLE boxes(
-				id INTEGER PRIMARY KEY, 
-				positive_classification INTEGER, 
-				image_id INTEGER, 
-				first_point_X REAL, 
-				first_point_Y REAL, 
-				second_point_X REAL, second_point_Y REAL)"
-				)
-				or die("create boxes failed");
-	
 	
 	$db->close();
 	
