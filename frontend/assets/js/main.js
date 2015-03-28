2e2c4a2b164bcab993e511720ab71cbe39535fb7
JeffOwOSun <jeffsun@jowos.moe>
2015-03-29 03:52:11 +0800

    further implemet canvas functions:

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,4 +1,5 @@
 (function(){
+	
 	var current_image = null;
 	var backgroundCanvas = document.getElementById("canvas0");
 	var drawingCanvas = document.getElementById("canvas1");
@@ -6,30 +7,25 @@
 	var users = [];
 	var isDrawing = false;
 	
+	drawingCanvas.addEventListener("mousedown", function(ev){console.log(ev);});
+	
 	function Point(x,y)
 	{
 		this.x = x;
 		this.y = y;
-	}
-	//draw canvas function
-	function drawCanvas()
-	{
-		//TODO: redrawImage
-		
-		//TODO: redrawStrokes
 	}
 	
 	function loadBackground()
 	{
 		//try to get the newest background from the server
 		$.ajax({
-			url: , //TODO: fill in the url
+			url: "get.php", //TODO: fill in the url
 		}).done(function(data)
 		{
 			//decode data
 			data = JSON.parse(data);
 			//the base64 string of image. image should not include the prefix
-			var image =
+			var image = data.image_str;
 			//draw the image onto
 			var ctx = backgroundCanvas.getContext("2d");
 			
@@ -43,39 +39,46 @@
 	function loadDrawing(userId)
 	{
 		$.ajax({
-			url: , //TODO: fill in the url
+			url: "get_user_canvas.php", //TODO: fill in the url
 			data: {
 				user_id: userId //the id of the user
 			}
-		}).done(function(data))
+		}).done(function(data)
 		{
 			//decode data
 			data = JSON.parse(data);
 			//the base64 string of the drawingCanvas
-			var image = ;
+			var image = data.image_str;
 			var ctx = drawingCanvas.getContext("2d");
 			ctx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
 			// load image from data url
 			var imageObj = new Image();
 			imageObj.src = "data:image/png;base64,"+image;
 			ctx.drawImage(imageObj,0,0);
-		}
+		})
 	}
 	
 	function loadUsers()
 	{
 		$.ajax({
-			url: , //TODO: fill in the url
-		}).done(function(data))
+			url: "get_user_list.php", //TODO: fill in the url
+		}).done(function(data)
 		{
 			//decode json data
 			data = JSON.decode(data);
 			
-			users = ; //TODO: set the user array with the retrieved value
+			users = data.user_list; //TODO: set the user array with the retrieved value
 			
 			//TODO: populate the user list with users
-		}
+		})
 	}
+	
+	drawingCanvas.addEventListener("mousedown", function(ev){startDrawing(new Point(ev.layerX,ev.layerY))});
+	drawingCanvas.addEventListener("touchstart", function(ev){startDrawing(new Point(ev.layerX,ev.layerY))});
+	drawingCanvas.addEventListener("mousemove", function(ev){drawContinue(new Point(ev.layerX,ev.layerY))});
+	drawingCanvas.addEventListener("touchstart", function(ev){drawContinue(new Point(ev.layerX,ev.layerY))});
+	drawingCanvas.addEventListener("mouseup", function(ev){stopDrawing(new Point(ev.layerX,ev.layerY))});
+	drawingCanvas.addEventListener("touchstop", function(ev){stopDrawing(new Point(ev.layerX,ev.layerY))});
 	
 	function startDrawing(point) {
 		isDrawing = true;
@@ -87,39 +90,48 @@
 	function drawContinue(point) {
 		if (isDrawing) {
 			var ctx = drawingCanvas.getContext("2d");
-			ctx.lineTo(pint.x, point.y);
+			ctx.lineTo(point.x, point.y);
 			ctx.stroke();
 		}
 	}
 	
 	function stopDrawing() {
 		isDrawing = false;
+		uploadUserCanvas();
+	}
+	
+	function uploadUserCanvas() {
 		//upload the resulting canvas to the webserver
-		var image = drawingCanvas.toDataURL(); //convert to base64 PNG fileCreatedDate
+		var image = forgeImage(); //convert to base64 PNG fileCreatedDate
 		$.ajax({
-			url: , //TODO: fill in the url
+			url: "upload_user_canvas.php",
 			data: {
-				image: image;
-			}
+				image_str: image,
+			},
+			type: "POST";
 		}).done(function(data){
 			//
 		})
 	}
 	
-	function getCanvasCoordinate(point) {
-		//transfer the coordinate value measured in the window to that in the canvas
-	}
-	
-	drawingCanvas.onmousedown = function(ev){console.log(ev);};
-	
-	function saveImage() {
+		
+	function forgeImage() {
 		//forge the image from background and drawing
 		var image = new Image();
+		var ctx = forgingCanvas.getContext("2d");
 		image.src = backgroundCanvas.toDataURL();
-		forgingCanvas.drawImage(image, 0, 0);
+		ctx.drawImage(image, 0, 0);
 		image.src = drawingCanvas.toDataURL();
-		forgingCanvas.drawImage(image, 0, 0);
-		
-		
+		ctx.drawImage(image, 0, 0);
+		return forgingCanvas.toDataURL();
 	}
+	
+	function downloadCanvas(link, imageURL, filename) {
+		link.href = imageURL;
+		link.download = filename;
+	}
+
+	document.getElementById('button-save').addEventListener('click', function() {
+		downloadCanvas(this, forgeImage(),'canvas.png');
+	}, false);
 })()
